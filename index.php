3aa2de14294577557a60040ea28d831996c4d1b6
ziaaflame <38154276+ziaaflame@users.noreply.github.com>
2018-07-25 11:35:36 +0600

    php4

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $number1 = 1000;
+    $number2 = 20;
+
+    echo $number1 + $number2;
+    echo '<br/>';
+    echo $number1 - $number2;
+    echo '<br/>';
+    echo $number1 * $number2;
+    echo '<br/>';
+    echo $number1 / $number2;
+    echo '<br/>';
+    echo $number1 % $number2;
+    echo '<br/>';
+    echo $number1 ** $number2;
+?>
